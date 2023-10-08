b1775bd16303e039c24a0b8cf553008049dd47ef
Yudis Sutisna <yudishan26@gmail.com>
2023-10-08 12:41:56 +0700

    membuat database migration, models dan controllers

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function show()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
