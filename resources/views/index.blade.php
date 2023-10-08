b86267d1d14a95755fe8765f213079de2bb27eb8
Yudis Sutisna <yudishan26@gmail.com>
2023-10-08 12:17:48 +0700

    menginstall tailwindcss

---
+++ resources/views/index.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <script src="https://kit.fontawesome.com/5b8ff677c4.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-gradient-to-r from-stone-950 to-slate-950 text-slate-200">
+    <nav class="h-16 border-b border-slate-800 px-4 flex justify-center">
+        <div class="container h-full flex items-center justify-between">
+            <div class="flex h-full items-center gap-12">
+                <h1 class="text-xl md:text-2xl font-semibold">NepBlog</h1>
+                <ul class="hidden md:flex gap-6">
+                    <li><a href="/dist/category.html" class="hover:text-white hover:underline">Home</a></li>
+                    <li><a href="/dist/category.html" class="hover:text-white hover:underline">Tutorial</a></li>
+                    <li><a href="/dist/category.html" class="hover:text-white hover:underline">Tips & Tricks</a></li>
+                    <li><a href="/dist/category.html" class="hover:text-white hover:underline">Opini</a></li>
+                    <li><a href="/dist/othercategory.html" class="hover:text-white hover:underline">Kategori
+                            Lainnya...</a>
+                    </li>
+                </ul>
+            </div>
+            <button><i class="fa-solid fa-magnifying-glass text-xl"></i></button>
+        </div>
+    </nav>
+
+    <main class="container m-auto my-8">
+        <div class="hidden lg:grid grid-cols-2 h-96 gap-2">
+            <div class="bg-slate-700 bg-opacity-30 border border-slate-800"></div>
+            <div class="grid grid-cols-2 gap-2">
+                <div class="bg-slate-700 bg-opacity-30 border border-slate-800"></div>
+                <div class="bg-slate-700 bg-opacity-30 border border-slate-800"></div>
+                <div class="bg-slate-700 bg-opacity-30 border border-slate-800"></div>
+                <div class="bg-slate-700 bg-opacity-30 border border-slate-800"></div>
+            </div>
+        </div>
+
+        <div class="mt-5 px-4 md:px-0">
+            <h2 class="text-lg">Semua Artikel</h2>
+
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-3">
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+                <div class="border border-slate-800 h-96 rounded-sm bg-opacity-30 bg-slate-700">
+                    <div class="bg-stone-950 h-3/5 flex justify-center items-center">ceritanya disini ada gambar
+                    </div>
+                    <div class="p-3">
+                        <h3 class="text-xl font-semibold">ini judul artikelnya, Lorem ipsum dolor sit amet.</h3>
+                        <p class="text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+                            perspiciatis...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
